8afed1d2dc1d39a8355564ec4aa7c09cd9a943a4
motomario <marius.kaz@icloud.com>
2024-02-05 15:15:44 +0200

    working scraper with info

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# https://github.com/JoMingyu/google-play-scraper/blob/master/README.md#google-play-scraper
+
 from google_play_scraper import reviews_all, Sort
 import pandas as pd
 import random
 import time
+
 
 def fetch_reviews_with_retry(app_id, max_retries=5, initial_delay=500):
     retries = 0
@@ -11,8 +14,8 @@ def fetch_reviews_with_retry(app_id, max_retries=5, initial_delay=500):
             reviews = reviews_all(
                 app_id,
                 sleep_milliseconds=random.randint(500, 2000),  # Random delay between requests
-                lang='en',  #or 'es', 'de'.. https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
-                country='gb',  #or 'gb', 'de', 'es'.. https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+                lang='en',  # or 'es', 'de'.. https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+                country='gb',  # or 'gb', 'de', 'es'.. https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
                 sort=Sort.NEWEST,
                 filter_score_with=None
             )
@@ -25,12 +28,14 @@ def fetch_reviews_with_retry(app_id, max_retries=5, initial_delay=500):
 
     raise Exception("Maximum retries reached. Failed to fetch reviews.")
 
+
 # Attempt to fetch reviews with error handling
 try:
-    reviews = fetch_reviews_with_retry('com.ovz.carscanner') #app id found in app link play.google.com/store/apps/details?id=
+    reviews = fetch_reviews_with_retry(
+        'com.ovz.carscanner')  # app id found in app link play.google.com/store/apps/details?id=
     reviews_df = pd.DataFrame(reviews)
     reviews_df.drop_duplicates(subset=['reviewId'], inplace=True)
-    reviews_df.to_csv('scrapes/com.ovz.carscanner_uk.csv', index=False) #change file name for new scrape
+    reviews_df.to_csv('scrapes/com.ovz.carscanner_uk.csv', index=False)  # change file name for new scrape
     print(f"Reviews successfully fetched and saved. Total reviews scraped: {len(reviews_df)}")
 except Exception as e:
     print(str(e))
